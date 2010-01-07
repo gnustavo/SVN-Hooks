858db4da0b995928e7a73ba5b5dbedc28e5a440e
gnustavo@gmail.com <gnustavo@gmail.com@d1a085d2-c0d6-11dd-ba5c-d3b7c2b3b534>
2010-01-07 01:59:49 +0000

    bumpping to version 0.24
    git-svn-id: https://svn-hooks.googlecode.com/svn/trunk@89 d1a085d2-c0d6-11dd-ba5c-d3b7c2b3b534

--- lib/SVN/Hooks.pm
+++ lib/SVN/Hooks.pm
@@ -15,11 +15,11 @@ SVN::Hooks - A framework for implementing Subversion hooks.
 
 =head1 VERSION
 
-Version 0.23
+Version 0.24
 
 =cut
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 =head1 SYNOPSIS
 
