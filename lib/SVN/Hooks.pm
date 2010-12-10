320bd808d8723f8a9272a03b5e05d3ab2974d9cd
gnustavo <gnustavo@d1a085d2-c0d6-11dd-ba5c-d3b7c2b3b534>
2010-12-10 17:32:27 +0000

    Implements the SVN::Hooks::Generic plugin
    git-svn-id: https://svn-hooks.googlecode.com/svn/trunk@118 d1a085d2-c0d6-11dd-ba5c-d3b7c2b3b534

--- lib/SVN/Hooks.pm
+++ lib/SVN/Hooks.pm
@@ -179,6 +179,10 @@ the C<tags> directory.
 Deny the addition of files which file names doesn't comply with a
 Regexp. Usually used to disallow some characteres in the filenames.
 
+=item SVN::Hooks::Generic
+
+This meta plugin allows for the easy creation of custom made hooks.
+
 =item SVN::Hooks::Notify
 
 Sends notification emails after successful commits.
@@ -336,7 +340,15 @@ sub run_hook {
 
     foreach my $conf (values %{$repo->{confs}}) {
 	if (my $hook = $conf->{$hook_name}) {
-	    $hook->($conf, @args);
+	    if (ref $hook eq 'CODE') {
+		$hook->($conf, @args);
+	    } elsif (ref $hook eq 'ARRAY') {
+		foreach my $h (@$hook) {
+		    $h->($conf, @args);
+		}
+	    } else {
+		die "SVN::Hooks: internal error!\n";
+	    }
 	}
     }
 
