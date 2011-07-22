e8ce6fa8cd6b6c1caa618502f3bcdc4d7c5be1f7
gustavo@gnustavo.com <gustavo@gnustavo.com@d1a085d2-c0d6-11dd-ba5c-d3b7c2b3b534>
2011-07-22 22:28:36 +0000

    bumpping to version 1.01
    git-svn-id: https://svn-hooks.googlecode.com/svn/trunk@175 d1a085d2-c0d6-11dd-ba5c-d3b7c2b3b534

--- lib/SVN/Hooks.pm
+++ lib/SVN/Hooks.pm
@@ -18,11 +18,11 @@ SVN::Hooks - A framework for implementing Subversion hooks.
 
 =head1 VERSION
 
-Version 1.00
+Version 1.01
 
 =cut
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 our @Conf_Files = (catfile('conf', 'svn-hooks.conf'));
 our $Repo       = undef;
